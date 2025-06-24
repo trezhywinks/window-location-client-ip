0d79d22a6b69b1a8a523e82242422c19b3aa6a85
trezhywinks <dreqxyonion@gmail.com>
2025-06-24 18:49:53 -0300

    update in npm.js

--- npm.js
+++ npm.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const colors = require('colors');
+const http = require('http');
 const qrcode = require('qrcode-terminal')
 const spooky = express();
 const port = "4040";
 const  host = "http://localhost:4040/"
 spooky.use(express.static('server'));
+
+spooky.get(('/server'), (req, res)  => {
+const userIp = req.headers['x-forwarded-for'] || req.ip;
+console.log('[!] UserIP => ', userIp.red.bold);
+//http.get(`http://ipinfo.io/${userIp}/geo`, (resp) => {
+http.get(`http://ip-api.com/json/${userIp}`, (resp) => {
+let data = '';
+
+resp.on('data', (chunk) => {
+data += chunk;
+});
+resp.on('end', () => {
+const parsedData = JSON.parse(data);
+const formattedData = JSON.stringify(parsedData, null, 2);
+console.log(formattedData);
+})
+})
+
+res.sendFile(__dirname + '/server/index.html');
+});
+
 
 qrcode.generate(host, {small: true}, function (qrcode){
 console.log(qrcode);
